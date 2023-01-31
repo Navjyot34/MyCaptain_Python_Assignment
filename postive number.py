7f6fa58a2a4bba91b6460e12d80b62ff879151ef
Navjyotsihdu34 <89522797+Navjyot34@users.noreply.github.com>
2023-01-31 21:59:24 +0530

    Python program to print all positive numbers in a range.

---
+++ postive number.py
@@ -0,0 +1,8 @@
+#Write a Python program to print all positive numbers in a range.
+
+
+myList= [-2,6,9,-120,100,20,40,-60,-80]
+for number in myList:
+    if number>=0:
+        print(number)
+        
