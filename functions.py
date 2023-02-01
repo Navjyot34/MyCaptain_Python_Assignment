4a7170e1691066fe78b821bf6886512d053b5339
Navjyotsihdu34 <89522797+Navjyot34@users.noreply.github.com>
2023-02-01 22:06:38 +0530

    python code of function that takes a string and prints the letters in decreasing order of frequency.

---
+++ functions.py
@@ -0,0 +1,31 @@
+#Write Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency.
+
+
+a='mississippi'
+print("Letters in decreasing order of frequency of word ",(a),"are=")
+
+myDict={"i":"4","s":"4","p":"2","m":"1"}
+
+
+def most_frequent(string):
+    print(string + (myDict["i"]))
+    
+most_frequent("i=")
+
+def most_frequent(string):
+    print(string + (myDict["s"]))
+    
+most_frequent("s=")
+
+def most_frequent(string):
+    print(string + (myDict["p"]))
+    
+most_frequent("p=")
+
+def most_frequent(string):
+    print(string + (myDict["m"]))
+    
+most_frequent("m=")
+
+  
+
